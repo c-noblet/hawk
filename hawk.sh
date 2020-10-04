968808158a7d90b9e7d7a0257001a20a40e72873
justin <justin@myfedora.home>
2020-10-04 16:38:10 +0200

    Fix all

--- hawk.sh
+++ hawk.sh
@@ -1,56 +1,71 @@
 #!/bin/bash
 
-. ./path.config
 
-if [[ $1 == "-wp" ]] ; then
-  wp = true
-  domainName = $2
-else 
-  domainName = $2
+############################################################
+#
+#  Description : Apache2 vhost generator
+#
+#  Auteur : Corentin Noblet
+#
+#  Date : 30/09/2020
+#
+###########################################################
+
+# Variable and Function
+
+
+# Create folders for domain
+function setFolders {
+  mkdir -p "/home/$USER/logs/${domainName}"
+  mkdir -p "/home/$USER/html/${domainName}"
+
+  chown -R "${USER}:${USER}" "/home/$USER/logs/${domainName}"
+  chown -R "${USER}:${USER}" "/home/$USER/html/${domainName}"
+}
+
+# Add and configuration for the new vhost
+function setApacheConf {
+  echo -e "<VirtualHost *:80>\n
+  ServerName local.${domainName}\n
+  ErrorLog /home/$USER/logs/${domainName}/error.log\n
+  CustomLog /home/$USER/logs/${domainName}/access.log common\n
+  DocumentRoot "/home/$USER/html/${domainName}/"\n
+  <Directory /home/$USER/html/${domainName}/>\n
+      Options -Indexes +FollowSymLinks +MultiViews\n
+      AllowOverride All\n
+      Require all granted\n
+  </Directory>\n
+ </VirtualHost>" > "/etc/apache2/sites-available/local.${domainName}.conf"
+
+
+  ln -s "/etc/apache2/sites-available/local.${domainName}.conf" "/etc/apache2/sites-enabled/local.${domainName}.conf"
+
+}
+
+# Add a Wordpress service
+function getWordpress {
+  wget -c https://fr.wordpress.org/latest-fr_FR.tar.gz -O "/home/$USER/html/wordpress.tar.gz"
+
+  tar -xzvf "/home/$USER/html/wordpress.tar.gz" -C "/home/$USER/html/${domainName}/"
+
+  rm "/home/$USER/html/wordpress.tar.gz"
+}
+
+###### Main code ######
+
+if [[ "$1" == "-wp" ]]; then
+  domainName="$2"
+  setFolders
+  setApacheConf
+  getWordpress
+else
+  domainName="$1"
+  setFolders
+  setApacheConf
 fi
 
-setApacheConf() {
-  confString = "<VirtualHost *:80>
-  ServerName local.${domainName}
-  ErrorLog ${logsPath}/${domainName}/error.log
-  CustomLog ${logsPath}/${domainName}/access.log common
-  DocumentRoot "${htmlPath}/${domainName}/"
-  <Directory ${htmlPath}/${domainName}/>
-      Options -Indexes +FollowSymLinks +MultiViews
-      AllowOverride All
-      Require all granted
-  </Directory>
-</VirtualHost>"
 
-  echo $confString >> "${a2AvailablePath}/local.${domainName}.conf"
+echo "127.0.0.1        local.${domainName}" >> "/etc/hosts"
 
-  ln -s "${a2AvailablePath}/local.${domainName}" "${a2EnabledPath}/local.${domainName}.conf"
-
-}
-
-getWordpress() {
-  wget -c https://fr.wordpress.org/latest-fr_FR.tar.gz -O "${htmlPath}/wordpress.tar.gz"
-
-  tar -xf "${htmlPath}/wordpress.tar.gz" -C "${htmlPath}/wordpress"
-
-  mv "${htmlPath}/wordpress" "${htmlPath}/${domainName}"
-
-  rm "${htmlPath}/wordpress.tar.gz"
-}
-
-setFolders() {
-  mkdir "${logsPath}/${domainName}"
-  mkdir "${htmlPath}/${domainName}"
-
-  chown -R "${user}:${user}" "${htmlPath}/${domainName}"
-  chown -R "${user}:${user}" "${logsPath}/${domainName}"
-}
-
-editHostsFile() {
-  echo "127.0.0.1        local.${domainName}" >> "/etc/hosts"
-}
-
-restartApache() {
-  systemctl restart apache2
-}
-
+echo "Your vhost is ready"
+echo -e "Now you can restart Apapche2 with \"systemctl restart apache2.service\""
