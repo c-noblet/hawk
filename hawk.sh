cf7829c614a6517f52a4cc35d5e2dda8db34503f
c-noblet <56131432+c-noblet@users.noreply.github.com>
2021-06-07 09:33:51 +0200

    Add node config and bug fix

--- hawk.sh
+++ hawk.sh
@@ -11,22 +11,23 @@
 #
 ###########################################################
 
-# Variable and Function
-custom_path=""
+# Variable and Functions
+custom_path="/"
+custom_user="user:user"
 
 # Create folders for domain
 function setFolders {
   mkdir -p "${custom_path}/${domainName}"
 
-  chown -R "${USER}:${USER}" "${custom_path}/${domainName}"
+  chown -R "${custom_user}" "${custom_path}/${domainName}"
 }
 
-# Add and configuration for the new vhost
+# Add configuration for the new vhost
 function setApacheConf {
   echo -e "<VirtualHost *:80>\n
   ServerName local.${domainName}\n
-  ErrorLog ${APACHE_LOG_DIR}/error.log\n
-  CustomLog ${APACHE_LOG_DIR}/access.log combined\n
+  ErrorLog \${APACHE_LOG_DIR}/error.log\n
+  CustomLog \${APACHE_LOG_DIR}/access.log combined\n
   DocumentRoot "${custom_path}/${domainName}/"\n
   <Directory ${custom_path}/${domainName}/>\n
       Options -Indexes +FollowSymLinks +MultiViews\n
@@ -38,7 +39,7 @@ function setApacheConf {
   ln -s "/etc/apache2/sites-available/local.${domainName}.conf" "/etc/apache2/sites-enabled/local.${domainName}.conf"
 }
 
-# Add and configuration for the new vhost for node.js
+# Add configuration for the new node.js vhost
 function setNodeApacheConf {
   echo -e "<VirtualHost *:80>\n
     ServerName local.${domainName}\n
@@ -68,34 +69,44 @@ function setNodeApacheConf {
 
 # Add a Wordpress service
 function getWordpress {
-  wget -c https://fr.wordpress.org/latest-fr_FR.tar.gz -O "/home/$USER/html/wordpress.tar.gz"
+  wget -c https://fr.wordpress.org/latest-fr_FR.tar.gz -O "${custom_path}/wordpress.tar.gz"
 
-  tar -xzvf "/${custom_path}/wordpress.tar.gz" -C "/${custom_path}/${domainName}/"
+  tar -xzvf "${custom_path}/wordpress.tar.gz" -C "${custom_path}/${domainName}/"
 
-  rm "/${custom_path}/wordpress.tar.gz"
+  rm "${custom_path}/wordpress.tar.gz"
+
+  mv $custom_path/$domainName/wordpress/* $custom_path/$domainName/
+  
+  rm -R "${custom_path}/${domainName}/wordpress"
+
+  chown -R "www-data:www-data" "${custom_path}/${domainName}/"
+
+  chown -R "${custom_user}" "${custom_path}/${domainName}/wp-content/themes"
+
+  chown "${custom_user}" "${custom_path}/${domainName}"
+
+  chmod -R 777 "${custom_path}/${domainName}"
 }
 
 ###### Main code ######
-  domainName="$1"
-  setFolders
-  
-  read -p "Install for Node.js" -n 1 -r
-  echo    # (optional) move to a new line
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
-  then
-    setNodeApacheConf
-  else
-	  setApacheConf
-  fi
-  
-  read -p "Install Wordpress ? " -n 1 -r
-  echo    # (optional) move to a new line
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
+domainName="$1"
+setFolders
+
+read -e -p "Install for Node.js ? [y/N] " REPLY
+if [ $REPLY = "Y" ] || [ $REPLY = "y" ]
+then
+  setNodeApacheConf
+else
+  setApacheConf
+
+  read -e -p "Install Wordpress ? [y/N] " REPLY2
+  if [ $REPLY2 = "Y" ] || [ $REPLY2 = "y" ]
   then
     getWordpress
   fi
+fi
 
-  systemctl restart apache2.service
+systemctl restart apache2.service
 
-echo "127.0.0.1        local.${domainName}" >> "/etc/hosts"
+echo "127.0.0.1       local.${domainName}" >> "/etc/hosts"
 echo "Your vhost is ready"
